f01627f753363acf10c0e2f65e3eed3c8bfad3c2
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-22 09:56:06 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,19 @@ module.exports = {
       },
       colors: {
         // colors
-        'blue-hover': '#00ddff',
+        'blue-hover': '#007DDB',
         'bgColorDark': '#121212',
         'cat-btn': '#232323',
         'cat-text': '#7B7B7B',
+        
+        'main-color': '#007DDB',
+        'dark-bg': '#151515',
+
+        'main-text': '#F6F6F6',
+        'sec-light-text': '#D2D6D7',
+        'cat-btn': '#232323',
+        'cat-text': '#7B7B7B',
+        'project-text': '#75757E',
       }
     },
   },
