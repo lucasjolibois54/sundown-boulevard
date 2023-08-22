0f12a8245f56b68feef6f3d90ed78ca8990f8e2d
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-22 13:04:57 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,65 @@
-import Image from "next/image";
+'use client'
+'use client'
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Hero from './components/home/Hero';
+import Subhero from './components/home/Subhero';
+import About from './components/home/About';
+
+export default function Home() {
+  const [theme, setTheme] = useState('dark-theme');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const aboutElem = document.getElementById('aboutComponent');
+      const buffer = 0.5 * window.innerHeight;
+
+      if (aboutElem) {
+        const rect = aboutElem.getBoundingClientRect();
+
+        if (rect.top <= window.innerHeight - buffer && rect.bottom >= 0) {
+          setTheme('light-theme');
+        } else {
+          setTheme('dark-theme');
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.body.className = theme;  // theme class
+    // transition
+    document.body.style.transition = 'background-color 0.3s ease, color 0.3s ease';
+  }, [theme]);
+
+  return (
+    <>
+      <main className="pb-56">
+        <Hero />
+        <div id="targetComponent" style={{ height: '150vh', zIndex: 0 }} />
+      </main>
+      
+      <Subhero />
+      <div id="aboutComponent">
+        <About />
+      </div>
+    </>
+  );
+}
+
+
+
+
+/*import Image from "next/image";
+
+import { useState, useEffect } from 'react';
 
 import Link from "next/link";
 import Hero from "./components/home/Hero";
@@ -8,9 +69,6 @@ export default function Home() {
   return (
     <>
       <main className="pb-56">
-        {/* <h2>Dashboard</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p> */}
-
         <Hero />
         <div style={{ height: "150vh", zIndex: 0 }} />
       </main>{" "}
@@ -18,3 +76,4 @@ export default function Home() {
     </>
   );
 }
+*/
