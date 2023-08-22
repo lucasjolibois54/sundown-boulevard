f5c71e674f04286ba30551d4ab8ca2da6dae0e26
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-22 09:56:14 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,20 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import Link from 'next/link'
-import Hero from './components/home/Hero'
-import Subhero from './components/home/Subhero'
+import Link from "next/link";
+import Hero from "./components/home/Hero";
+import Subhero from "./components/home/Subhero";
 
 export default function Home() {
   return (
-    <main className='pb-56'>
-      {/* <h2>Dashboard</h2>
+    <>
+      <main className="pb-56">
+        {/* <h2>Dashboard</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p> */}
 
-      <Hero/>
-      <div style={{ height: '150vh', background: 'lightgray', zIndex: 0 }}>
-        Scroll down to see the image grow in width.
-      </div>
-      <Subhero/>
-    </main>
-  )
+        <Hero />
+        <div style={{ height: "150vh", zIndex: 0 }} />
+      </main>{" "}
+      <Subhero />
+    </>
+  );
 }
