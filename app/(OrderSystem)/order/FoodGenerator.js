aa26b663165dcbccc58631406a4db500aea86c43
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-23 09:48:29 +0200

    Update FoodGenerator.js

--- app/(OrderSystem)/order/FoodGenerator.js
+++ app/(OrderSystem)/order/FoodGenerator.js
@@ -1,39 +1,78 @@
+'use client';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+
+
 async function getData() {
     const res = await fetch('https://www.themealdb.com/API/JSON/V1/1/RANDOM.PHP/', {
         next: {
             revalidate: 0,
         }
-      })
-    // The return value is *not* serialized
-    // I can return Date, Map, Set, etc.
-   
+    });
+
     if (!res.ok) {
-      // Activate closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data')
+        throw new Error('Failed to fetch data');
     }
-   
-    return res.json()
-        /*.then(data => {
-        console.log(data);
-        return data;
-      });*/
-  }
-   
-  export default async function FoodGenerator() {
-    const data = await getData()
-   
+
+    return res.json();
+}
+
+export default function FoodGenerator() {
+    const [mealData, setMealData] = useState(null);
+    const [email, setEmail] = useState('');
+
+    useEffect(() => {
+        // Fetch initial data when component mounts
+        (async () => {
+            const data = await getData();
+            setMealData(data.meals);
+        })();
+    }, []);
+
+    const handleGenerateNewMeal = async () => {
+        const data = await getData();
+        setMealData(data.meals);
+    };
+
+    const handleSaveData = () => {
+        if (mealData && mealData.length > 0 && email) {
+            const savedData = {
+                email: email,
+                mealName: mealData[0].strMeal
+            };
+            localStorage.setItem('savedEmail', email); // Store email separately for easy retrieval
+            localStorage.setItem(email, JSON.stringify(savedData));
+        }
+    };
+    
+
     return (
         <main>
-            {data.meals.map((meal) => (
+            <div>
+                <input 
+                    type="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    placeholder="Enter your email" 
+                />
+                <button onClick={handleSaveData}>Save Meal</button>
+            </div>
+            {mealData && mealData.map((meal) => (
                 <div key={meal.idMeal}>
-                    {/* data for meals */}
                     <h2>{meal.strMeal}</h2>
                     <img src={meal.strMealThumb} alt={meal.strMeal} />
                 </div>
             ))}
-            {data.length === 0 && (
-<p className="text-center">There are no dishes available, damn!</p>
-)}
+            {!mealData && (
+                <p className="text-center">Fetching data...</p>
+            )}
+            {mealData && mealData.length === 0 && (
+                <p className="text-center">There are no dishes available, damn!</p>
+            )}
+            <button onClick={handleGenerateNewMeal}>Generate New Meal</button>
+            <Link href="/order/drinks">
+                Next
+            </Link>
         </main>
     );
 }
