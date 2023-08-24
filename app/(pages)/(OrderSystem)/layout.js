a48a963a47169a9f7e3288a767a6cf5c0fbfd741
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-24 18:53:11 +0200

    Update layout.js

--- app/(pages)/(OrderSystem)/layout.js
+++ app/(pages)/(OrderSystem)/layout.js
@@ -3,6 +3,5 @@ import Navbar from "../../components/main/Navbar"
 export default function OrderLayout({ children }) {
     return <section>
         {children}
-        <h1>Order Layout</h1>
         </section>
   }
