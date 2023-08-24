1a782a3c3f5e043d47db92d547b82f1f22ec696f
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-24 14:29:16 +0200

    Create SelectedDrink.js

---
+++ app/components/order/SelectedDrink.js
@@ -0,0 +1,47 @@
+'use client';
+import { useEffect, useRef } from 'react';
+
+const SelectedDrink = () => {
+  const circleRef = useRef(null);
+
+  useEffect(() => {
+    const degreeToRadian = (angle) => {
+      return angle * (Math.PI / 180);
+    };
+
+    const radius = 80;
+    const diameter = radius * 2;
+
+    const circle = circleRef.current;
+    circle.style.width = `${diameter}px`;
+    circle.style.height = `${diameter}px`;
+
+    const text = circle.dataset.text;
+    const characters = text.split("");
+
+    const deltaAngle = 360 / characters.length;
+    const characterOffsetAngle = 8;
+    let currentAngle = -90;
+
+    characters.forEach((character, index) => {
+      const span = document.createElement("span");
+      span.innerText = character;
+      const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+      const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+
+      const transform = `translate(${xPos}px, ${yPos}px)`;
+      const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
+      span.style.transform = `${transform} ${rotate}`;
+
+      currentAngle += deltaAngle;
+      circle.appendChild(span);
+    });
+
+  }, []);
+
+  return (
+    <div id="circle" className='text-white' ref={circleRef} data-text="BEER CHOSEN - BEER CHOSEN -"></div>
+  );
+}
+
+export default SelectedDrink;
