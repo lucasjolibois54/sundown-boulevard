900a6f49c4be7e33241ecf2a0ce86f100a36adad
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-24 15:39:22 +0200

    Update layout.js

--- app/(pages)/(OrderSystem)/layout.js
+++ app/(pages)/(OrderSystem)/layout.js
@@ -1,7 +1,7 @@
 import Navbar from "../../components/main/Navbar"
 
 export default function OrderLayout({ children }) {
-    return <section className="max-w-6xl mx-auto">
+    return <section>
         {children}
         <h1>Order Layout</h1>
         </section>
