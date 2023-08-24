4e288b267a76f2e8b711c045a187bfd737f7ff84
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-24 15:39:13 +0200

    Create layout.js

---
+++ app/(pages)/(OrderSystem)/order/drinks/layout.js
@@ -0,0 +1,7 @@
+export default function OrderLayout({ children }) {
+    return <section className="bg-bgColorDark">
+        <div className="max-w-6xl mx-auto">
+        {children}
+        <h1>Order Layout</h1></div>
+        </section>
+  }
