533c85885c1fd8be1e6e0e93ddf00ebe27c8dee7
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-25 10:40:51 +0200

    Update page.js

--- app/(pages)/(OrderSystem)/order/drinks/page.js
+++ app/(pages)/(OrderSystem)/order/drinks/page.js
@@ -230,7 +230,7 @@ export default function Drink() {
           onClick={() => setVisibleDrinks((prev) => prev + 6)}
           class="relative inline-flex items-center justify-center px-7 py-2 overflow-hidden font-mono font-medium tracking-tighter text-white bg-transparent border-gray-400 border-2 hover:border-bgColorDark rounded-lg group"
         >
-          <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-gray-500 rounded-full group-hover:w-72 group-hover:h-72"></span>
+          <span class="absolute w-0 h-0 transition-all duration-1000 ease-out bg-gray-500 rounded-full group-hover:w-72 group-hover:h-72"></span>
           <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
           <span class="relative">View More</span>
         </button>
