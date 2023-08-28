8457bb3dd2ba8d6ad70ff9ca0fdd6845390a6c6a
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-28 11:18:59 +0200

    Update page.js

--- app/(pages)/(OrderSystem)/order/date/page.js
+++ app/(pages)/(OrderSystem)/order/date/page.js
@@ -42,7 +42,7 @@ export default function TimePicker() {
   
       savedData = {
         ...savedData,
-        date: selectedDate.toISOString().split('T')[0], 
+        date: moment(selectedDate).format('YYYY-MM-DD'),
         time: selectedTime.format('HH:mm'),
         customer: customerCount,
       };
@@ -72,7 +72,7 @@ export default function TimePicker() {
 
       <div className="p-8 rounded-xl shadow-2xl space-y-8 w-full bg-white bg-opacity-10 backdrop-blur-md">
         <div className="react-calendar shadow-lg rounded-lg overflow-hidden">
-          <Calendar
+          {/* <Calendar
             localizer={localizer}
             defaultDate={new Date()}
             defaultView="month"
@@ -90,7 +90,74 @@ export default function TimePicker() {
                 };
               }
             }}
+          /> */}
+          <Calendar
+  localizer={localizer}
+  defaultDate={new Date()}
+  defaultView="month"
+  views={['month']}
+  events={[]}
+  style={{ height: "700px" }}
+  onSelectSlot={handleDateSelect}
+  selectable={true}
+  dayPropGetter={(date) => {
+    if (selectedDate && moment(date).isSame(selectedDate, 'day')) {
+      return {
+        style: {
+          backgroundColor: '#007DDB',
+        },
+      };
+    }
+  }}
+  components={{
+    toolbar: (toolbarProps) => {
+      return (
+        <div className="rbc-toolbar">
+          <span className="rbc-btn-group">
+            <button onClick={() => toolbarProps.onNavigate('PREV')}>Back</button>
+            <button onClick={() => toolbarProps.onNavigate('NEXT')}>Next</button>
+          </span>
+          <span className="rbc-toolbar-label">{toolbarProps.label}</span>
+          <span className="rbc-btn-group !text-gray-900 text-center">
+            <div className='flex gap-3'>
+          <div className="flex items-center space-x-4">
+          <label className="text-gray-300 font-medium mb-2 mr-2">Customer:</label>
+          <button
+            onClick={() => setCustomerCount(prev => prev - 1)}
+            className="!bg-gray-700 !text-white !rounded !px-3 !py-2 !hover:bg-gray-600 !active:bg-gray-800 !focus:outline-none !focus:ring !focus:ring-gray-200 !transition-shadow !shadow-md"
+          >
+            -
+          </button>
+          <input
+            type="number"
+            value={customerCount}
+            readOnly
+            className="border rounded px-3 py-2 w-16 text-center shadow-md"
           />
+          <button
+            onClick={() => setCustomerCount(prev => prev + 1)}
+            className="!bg-gray-700 !text-white !rounded !px-3 !py-2 !hover:bg-gray-600 !active:bg-gray-800 !focus:outline-none !focus:ring !focus:ring-gray-200 !transition-shadow !shadow-md"
+          >
+            +
+          </button>
+        </div>
+        <Link
+              href="/order/receipt"
+              onClick={handleSaveDateTime}
+              class="hover:cursor-none relative inline-flex items-center justify-center px-7 py-2 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 border-gray-800 border-2 hover:BORDER-bgColorDark rounded-lg group"
+            >
+              <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-main-color rounded-full group-hover:w-72 group-hover:h-72"></span>
+              <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+              <span class="relative">Complete Order</span>
+            </Link>
+        </div>
+          </span>
+        </div>
+      );
+    }
+  }}
+/>
+
         </div>
         
         {/* Modal for time slot selection */}
@@ -109,7 +176,7 @@ export default function TimePicker() {
           </div>
         )}
 
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 hidden">
           <label className="text-gray-300 font-medium mb-2 mr-2">Customer:</label>
           <button
             onClick={() => setCustomerCount(prev => prev - 1)}
@@ -133,7 +200,7 @@ export default function TimePicker() {
         
         <button
           onClick={handleSaveDateTime}
-          className="w-full mt-4 bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-200 transition-shadow shadow-md"
+          className=" w-full mt-4 bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-200 transition-shadow shadow-md"
         >
           Save Date & Time
         </button>
