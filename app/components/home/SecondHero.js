065eb1f1c479d9195c6a230d12404394adb221b2
lucasjolibois54 <lucasjolibois@icloud.com>
2023-08-28 22:01:28 +0200

    Update SecondHero.js

--- app/components/home/SecondHero.js
+++ app/components/home/SecondHero.js
@@ -19,18 +19,22 @@ export default function SubHero() {
       onMouseLeave={() => {
         setCursorText("");
         setCursorVariant("default");
-      }} className="relative w-full h-[calc(100vh-60px)]" onMouseMove={handleMouseMove}>
-      <img src="https://res.cloudinary.com/debkwdctz/image/upload/v1693229110/lucasjolibois54_a_bar_restaurant_random_color_design_in_the_sty_5f35697c-299e-42a9-bdab-16dcb98babbf-transformed_1_lfh1dd.png" alt="Hero" className="w-full h-full object-cover" />
+      }} className="relative flex items-center justify-center w-full h-[calc(100vh-60px)]" onMouseMove={handleMouseMove}>
+      
+      <img src="https://res.cloudinary.com/debkwdctz/image/upload/v1693229110/lucasjolibois54_a_bar_restaurant_random_color_design_in_the_sty_5f35697c-299e-42a9-bdab-16dcb98babbf-transformed_1_lfh1dd.png" alt="Hero" className="w-full h-full object-cover absolute inset-0 z-0" />
 
       <motion.div
-        className="absolute inset-0 bg-black opacity-100"
+        className="absolute inset-0 bg-black opacity-100 z-10"
         initial={false}
         animate={{
           maskImage: `radial-gradient(circle 200px at ${cursorPosition.x}px ${cursorPosition.y}px, transparent 0%, black 100%)`,
           WebkitMaskImage: `radial-gradient(circle 200px at ${cursorPosition.x}px ${cursorPosition.y}px, transparent 0%, black 100%)`
         }}
       ></motion.div>
+
+      <h1 className="z-20 text-white text-center text-5xl">Where <b>Memories</b> are Poured <br/>and Moments are <b>Savored</b></h1>
     </div>
   );
 }
+
 
